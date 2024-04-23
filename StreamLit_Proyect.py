176e8b6d5266fd89ba2b4b7b58802e0d142bfccf
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-23 18:37:29 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -3,3 +3,5 @@ import pandas as pd
 
 st.write("¡Hola Mundo!")
 st.title("Mi Aplicación Streamlit")
+st.sidebar.header("Configuración")
+option = st.sidebar.selectbox("Seleccione una opción", ["Opción 1", "Opción 2", "Opción 3"])
