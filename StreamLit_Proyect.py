3a1895a409de80da87af119a0eabd90dac34822e
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-25 11:01:01 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,55 +1,27 @@
 import streamlit as st
-import sqlite3
-# Sidebar
-st.sidebar.title("Navegación")
-opcion = st.sidebar.radio("Selecciona una opción", ["Inicio", "Iniciar sesión"])
+import csv
 
-# Título centrado
-st.write("""
-<div style='text-align: center;'>
-    <h1>Mi Aplicación</h1>
-</div>
-""", unsafe_allow_html=True)
+# Función para autenticar usuarios desde un archivo CSV
+def authenticate_user(username, password):
+    with open('uss_pass.csv', 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            if row['username'] == username and row['password'] == password:
+                return True
+    return False
 
-# Página de inicio de sesión
-if opcion == "Iniciar sesión":
-    st.write("## Inicio de sesión")
-    usuario = st.text_input("Usuario")
-    contraseña = st.text_input("Contraseña", type="password")
-    if st.button("Iniciar sesión"):
-        # Aquí puedes agregar la lógica para autenticar al usuario
-        st.success("Inicio de sesión exitoso")
+def main():
+    st.title("Autenticación de Usuarios")
 
-# Página de inicio
-elif opcion == "Inicio":
-    st.write("## Bienvenido a mi aplicación")
-    # Aquí puedes agregar el contenido de la página de inicio
+    username = st.text_input("Nombre de Usuario")
+    password = st.text_input("Contraseña", type="password")
 
-def verificar_credenciales(usuario, contraseña):
-    # Conectar a la base de datos
-    conn = sqlite3.connect('mydatabase.db')
-    c = conn.cursor()
+    if st.button("Iniciar Sesión"):
+        if username and password:
+            if authenticate_user(username, password):
+                st.success(f"Bienvenido, {username}!")
+            else:
+                st.error("Credenciales incorrectas. Por favor, inténtalo de nuevo.")
 
-    # Consultar las credenciales del usuario
-    c.execute("SELECT * FROM usuarios WHERE usuario = ? AND contraseña = ?", (usuario, contraseña))
-    resultado = c.fetchone()
-
-    # Cerrar la conexión
-    conn.close()
-
-    # Si se encontró un resultado, las credenciales son válidas
-    if resultado:
-        return True
-    else:
-        return False
-
-if opcion == "Iniciar sesión":
-    st.write("## Inicio de sesión")
-    usuario = st.text_input("Usuario")
-    contraseña = st.text_input("Contraseña", type="password")
-    if st.button("Iniciar sesión"):
-        if verificar_credenciales(usuario, contraseña):
-            # Redirigir a la nueva pestaña
-            st.session_state.autenticado = True
-        else:
-            st.error("Credenciales inválidas")
+if __name__ == "__main__":
+    main()
