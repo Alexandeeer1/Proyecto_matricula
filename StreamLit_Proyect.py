4bad4759f9759fbd55caac363db8b63f38f8fec0
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-25 11:06:07 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -7,28 +7,26 @@ from io import StringIO
 csv_url = 'https://raw.githubusercontent.com/Alexandeeer1/Proyecto_matricula/main/uss_pass.csv'
 
 # Función para autenticar usuarios desde un archivo CSV en línea
-def authenticate_user(Usuarios, Contraseña):
-    # Descargar el contenido del archivo CSV desde la URL
+def authenticate_user(username, password):
     response = requests.get(csv_url)
     if response.status_code == 200:
-        # Leer el contenido como un archivo CSV utilizando StringIO
         csv_data = StringIO(response.text)
         csv_reader = csv.DictReader(csv_data)
         for row in csv_reader:
-            if row['Usuarios'] == Usuarios and row['Contraseña'] == Contraseña:
+            if row['Usuarios'] == username and row['Contraseña'] == password:
                 return True
     return False
 
 def main():
     st.title("Autenticación de Usuarios")
 
-    Usuarios = st.text_input("Nombre de Usuario")
-    Contraseña = st.text_input("Contraseña", type="Contraseña")
+    username = st.text_input("Nombre de Usuario")
+    password = st.text_input("Contraseña", type="password")
 
     if st.button("Iniciar Sesión"):
-        if Usuarios and Contraseña:
-            if authenticate_user(Usuarios, Contraseña):
-                st.success(f"Bienvenido, {Usuarios}!")
+        if username and password:
+            if authenticate_user(username, password):
+                st.success(f"Bienvenido, {username}!")
             else:
                 st.error("Credenciales incorrectas. Por favor, inténtalo de nuevo.")
 
