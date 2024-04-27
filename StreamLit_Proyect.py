30df9215f9406f72c88a78a743a1cdc02f5a77c4
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 19:50:18 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>Mi Página en Streamlit</h1>", unsafe_allow_html=True)
+
+url_imagen = "https://intranet.upch.edu.pe/account/static/img/upch/logo/isotipo.jpg"
+st.image(url_imagen, caption='Universidad Peruana Cayetano Heredia', use_column_width=True)
