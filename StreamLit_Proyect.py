1f5ee0933a0a3dfa85ad1c7d5649c97235ce92d5
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 20:13:19 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -27,10 +27,27 @@ def login_page():
 def show_logged_in_section():
     st.sidebar.title("Menú")
     selected_section = st.sidebar.selectbox("Seleccione una opción", ["Inicio", "Perfil", "Otra sección"])
-    if selected_section == "Perfil":
-        st.write("Aquí puedes ver y editar tu perfil")
+    if selected_section == "Inicio":
+        show_home_page()
+    elif selected_section == "Perfil":
+        show_profile_page()
     elif selected_section == "Otra sección":
-        st.write("Aquí puedes agregar el contenido de otra sección")
+        show_other_page()
+
+# Función para mostrar la página de inicio
+def show_home_page():
+    st.markdown("<h2 style='text-align: center;'>Bienvenido a la página de inicio</h2>", unsafe_allow_html=True)
+    st.write("Aquí puedes agregar el contenido de la página de inicio")
+
+# Función para mostrar la página de perfil
+def show_profile_page():
+    st.markdown("<h2 style='text-align: center;'>Perfil de usuario</h2>", unsafe_allow_html=True)
+    st.write("Aquí puedes ver y editar tu perfil")
+
+# Función para mostrar otra página
+def show_other_page():
+    st.markdown("<h2 style='text-align: center;'>Otra sección</h2>", unsafe_allow_html=True)
+    st.write("Aquí puedes agregar el contenido de otra sección")
 
 # Mostrar la página de inicio de sesión por defecto
 login_page()
