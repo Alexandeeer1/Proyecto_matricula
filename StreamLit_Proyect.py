38e6d4f51480093d92522f044b1a1b672ece0eeb
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 19:42:35 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 # Título centrado y más pequeño
 st.title("Mi Página en Streamlit")
-st.markdown("<h1 style='text-align: center; color: black;'>Mi Página en Streamlit</h1>", unsafe_allow_html=True)
 
 # Mostrar la imagen
 imagen_url = "https://intranet.upch.edu.pe/account/static/img/upch/logo/isotipo.jpg"
