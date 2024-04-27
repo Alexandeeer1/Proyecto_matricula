a63b1093151cc56ebbdd2d3e94e56037be360c2f
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 19:36:28 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,33 +1,45 @@
 import streamlit as st
 
-# Establecer el estilo CSS para el título y la imagen
+# Establecer el estilo CSS para el diseño principal y el contenedor interno
 st.markdown(
     """
     <style>
-    .imagen-container {
+    .layout-container {
+        width: 100%;
+        height: 100vh; /* Altura total de la ventana */
         display: flex;
         justify-content: center;
+        align-items: center;
     }
 
-    .imagen {
-        width: 50%; /* Ancho de la imagen */
-        border-radius: 10px; /* Radio de borde de la imagen */
-    }
-
-    .titulo {
-        text-align: center; /* Centrar el texto del título */
+    .content-container {
+        max-width: 80%; /* Ancho máximo del contenido */
+        padding: 20px; /* Espaciado interno del contenido */
+        background-color: #f0f0f0; /* Color de fondo del contenido */
+        border-radius: 10px; /* Radio de borde del contenido */
     }
     </style>
     """,
     unsafe_allow_html=True
 )
 
-# Añadir la imagen centrada
+# Contenedor principal que ocupa toda la pantalla
+st.markdown('<div class="layout-container">', unsafe_allow_html=True)
+
+# Contenedor interno para el contenido
+st.markdown('<div class="content-container">', unsafe_allow_html=True)
+
+# Agrega tus elementos dentro de este contenedor
 imagen_url = "https://intranet.upch.edu.pe/account/static/img/upch/logo/isotipo.jpg"
 st.markdown(
-    f'<div class="imagen-container"><img src="{imagen_url}" class="imagen"></div>',
+    f'<div style="text-align:center"><img src="{imagen_url}" style="width: 50%; border-radius: 10px;"></div>',
     unsafe_allow_html=True
 )
 
-# Añadir el título centrado utilizando HTML y CSS
-st.markdown('<h1 class="titulo">Portal de Matrícula</h1>', unsafe_allow_html=True)
+st.title("Portal de Matrícula",  class="titulo")
+
+# Cierre del contenedor interno
+st.markdown('</div>', unsafe_allow_html=True)
+
+# Cierre del contenedor principal
+st.markdown('</div>', unsafe_allow_html=True)
