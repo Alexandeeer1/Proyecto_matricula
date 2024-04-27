05837a76bac6d40857a3a2bad5c2808f1b900eb0
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 20:54:00 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -43,8 +43,6 @@ def show_logged_in_content(username):
     # Mostrar el DataFrame filtrado
     st.write(filtered_df)
 
-# Mostrar la página de inicio de sesión
-show_logged_in_content("Usuario")
 
 
 # Mostrar la página de inicio de sesión por defecto
