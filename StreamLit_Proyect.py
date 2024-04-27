300977c32a27ff90a7b7138ba8e26ad24e8c46b9
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 19:23:37 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Añadir la imagen centrada
-imagen_url = "https://is2-ssl.mzstatic.com/image/thumb/Purple126/v4/b9/3d/cb/b93dcb5d-0816-86f7-ff52-f9e8d59c5abd/AppIcon-0-1x_U007emarketing-0-5-0-sRGB-85-220.png/1200x630wa.png"
+imagen_url = "https://intranet.upch.edu.pe/account/static/img/upch/logo/isotipo.jpg"
 st.markdown(
     f'<div style="text-align:center"><img src="{imagen_url}" width="100"></div>',
     unsafe_allow_html=True
