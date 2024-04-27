c3f9c1cdcbbdcd4810daedeb568a1f88775c0f54
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 19:34:20 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,26 +1,29 @@
 import streamlit as st
 
-# Establecer el estilo CSS para el título
+# Establecer el estilo CSS para la imagen
 st.markdown(
     """
     <style>
-    .titulo {
-        background-color: #f0f0f0; /* Color de fondo del título */
-        color: #333333; /* Color del texto del título */
-        padding: 10px; /* Espaciado interno del título */
-        border-radius: 10px; /* Radio de borde del título */
+    .imagen-container {
+        display: flex;
+        justify-content: center;
+    }
+
+    .imagen {
+        width: 50%; /* Ancho de la imagen */
+        border-radius: 10px; /* Radio de borde de la imagen */
     }
     </style>
     """,
     unsafe_allow_html=True
 )
 
-# Añadir la imagen con el tamaño ajustado
+# Añadir la imagen centrada
 imagen_url = "https://intranet.upch.edu.pe/account/static/img/upch/logo/isotipo.jpg"
 st.markdown(
-    f'<img src="{imagen_url}" class="imagen" style="width: 50%;">',
+    f'<div class="imagen-container"><img src="{imagen_url}" class="imagen"></div>',
     unsafe_allow_html=True
 )
 
-# Añadir el título con la clase CSS personalizada
-st.markdown('<h1 class="titulo">Portal de Matrícula</h1>', unsafe_allow_html=True)
+# Añadir el título
+st.title("Portal de Matrícula")
