dc851f1ea587d32216e71462cb7ab89401d7cdc5
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 19:20:45 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 # Añadir la imagen centrada
 imagen_url = "https://portalmatricula.cayetano.pe/static/media/logo-icon.134f0cc399e26b0cfaa0.png"
-st.image(imagen_url, width=100, align='center')  # Ajusta el ancho de la imagen según sea necesario
+st.markdown(
+    f'<div style="text-align:center"><img src="{imagen_url}" width="100"></div>',
+    unsafe_allow_html=True
+)
 
 # Añadir el título
 st.title("Portal de\nMatrícula")
