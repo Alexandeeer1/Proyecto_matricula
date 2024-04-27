26c46f185888a8ac8e45a8ec5d78f83207c09c15
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 19:45:24 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,8 +1,6 @@
 import streamlit as st
 
-# Título centrado y más pequeño
-st.title("Mi Página en Streamlit")
+st.title("Mi Página en Streamlit").align("center")
 
-# Mostrar la imagen más pequeña y centrada
 imagen_url = "https://intranet.upch.edu.pe/account/static/img/upch/logo/isotipo.jpg"
 st.image(imagen_url, caption='Logo de UPCH', width=200, use_column_width=True)
