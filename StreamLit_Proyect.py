c84c02229261a215c30e805e8c487e67e4fea7a2
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 20:27:34 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+url_csv = "https://raw.githubusercontent.com/Alexandeeer1/Proyecto_matricula/7cd2140758281c22508e80f81d4e78c34360d983/uss_pass.csv"
+df = pd.read_csv(url_csv)
+st.markdown("<h1 style='text-align: center;'>PORTAL DE MATRICULA CAYETANO</h1>", unsafe_allow_html=True)
+url_imagen = "https://intranet.upch.edu.pe/account/static/img/upch/logo/isotipo.jpg"
+st.markdown(f'<div style="text-align: center; border-radius: 20px; overflow: hidden; margin-bottom: 50px;"><img src="{url_imagen}" width="200" style="border-radius: 20px;"></div>', unsafe_allow_html=True)
+
+#####################################################################################
+# Página de inicio de sesión
 def login_page():
     st.markdown("<h2 style='text-align: center;'>INICIE SESIÓN</h2>", unsafe_allow_html=True)
     with st.form(key='login_form'):
@@ -18,14 +26,7 @@ def login_page():
 # Función para mostrar la nueva sección después de iniciar sesión
 def show_logged_in_content(username):
     st.markdown(f"<h2 style='text-align: center;'>BIENVENIDO, {username}</h2>", unsafe_allow_html=True)
-    st.markdown("<h6 style='text-align: center;'>En este apartado podrás verificar, ver, y coordinar los cursos cupos y disponibilidad</h6>", unsafe_allow_html=True)
-    
-    st.header("Archivo CSV - PLAN DE ESTUDIOS")
-    
-    url_csv = "https://raw.githubusercontent.com/Alexandeeer1/Proyecto_matricula/94a5f65b0920df8b0f0ad31270c101d381f62546/PLAN_DE_ESTUDIOS.csv"
-    df = pd.read_csv(url_csv)
-    
-    st.write(df)
+    st.markdown("<h6 style='text-align: center;'>En este apartado podras verificar, ver, y coordinar los cursos cupos y disponibilidad</h6>", unsafe_allow_html=True)
 
 # Mostrar la página de inicio de sesión por defecto
 login_page()
