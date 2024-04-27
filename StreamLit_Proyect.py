7cd2140758281c22508e80f81d4e78c34360d983
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 19:55:57 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -3,18 +3,20 @@ import streamlit as st
 st.markdown("<h1 style='text-align: center;'>Mi Página en Streamlit</h1>", unsafe_allow_html=True)
 
 url_imagen = "https://intranet.upch.edu.pe/account/static/img/upch/logo/isotipo.jpg"
-st.markdown(f'<div style="text-align: center; border-radius: 20px; overflow: hidden;"><img src="{url_imagen}" width="200" style="border-radius: 20px;"></div>', unsafe_allow_html=True)
+st.markdown(f'<div style="text-align: center; border-radius: 20px; overflow: hidden; margin-bottom: 50px;"><img src="{url_imagen}" width="200" style="border-radius: 20px;"></div>', unsafe_allow_html=True)
 
-st.markdown("<h2 style='text-align: center; margin-top: 50px;'>Iniciar Sesión</h2>", unsafe_allow_html=True)
-#############################################################################################################################
+st.markdown("<h2 style='text-align: center;'>Iniciar Sesión</h2>", unsafe_allow_html=True)
+
 # Formulario de login
-username = st.text_input("Usuario")
-password = st.text_input("Contraseña", type="password")
+with st.form(key='login_form'):
+    username = st.text_input("Usuario")
+    password = st.text_input("Contraseña", type="password")
+    submit_button = st.form_submit_button(label='Iniciar Sesión')
 
-if st.button("Iniciar Sesión"):
-    # Aquí puedes agregar la lógica para verificar el usuario y contraseña
-    # Por ejemplo, puedes comparar los valores ingresados con credenciales predefinidas
-    if username == "usuario" and password == "contraseña":
-        st.success("¡Inicio de sesión exitoso!")
-    else:
-        st.error("Usuario o contraseña incorrectos")
+    if submit_button:
+        # Aquí puedes agregar la lógica para verificar el usuario y contraseña
+        # Por ejemplo, puedes comparar los valores ingresados con credenciales predefinidas
+        if username == "usuario" and password == "contraseña":
+            st.success("¡Inicio de sesión exitoso!")
+        else:
+            st.error("Usuario o contraseña incorrectos")
