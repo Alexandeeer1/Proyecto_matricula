b61dc1d90653b056467929f43766923da8074ee9
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 19:48:13 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
-st.title("Mi Página en Streamlit").align("center")
+title_text = "Mi Página en Streamlit"
+st.title(title_text.center(len(title_text) + 6))  # Añade espacio extra para centrar
