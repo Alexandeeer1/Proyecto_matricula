a6572e4d1529b8e8e9226d160372288c56a92321
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 19:19:13 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,6 +1,8 @@
-
 import streamlit as st
 
-st.title("Portal")
-st.title("de")
-st.title("Matrícula")
+# Añadir la imagen
+imagen_url = "https://portalmatricula.cayetano.pe/static/media/logo-icon.134f0cc399e26b0cfaa0.png"
+st.image(imagen_url, width=100)  # Ajusta el ancho de la imagen según sea necesario
+
+# Añadir el título
+st.title("Portal de\nMatrícula")
