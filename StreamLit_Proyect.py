67e7e37ce8b7e1c9f28ddf6e4538ae902e059dea
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 19:20:01 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Añadir la imagen
-imagen_url = "https://portalmatricula.cayetano.pe/static/media/logo-icon.134f0cc399e26b0cfaa0.png"
+imagen_url = "https://staticfileszyxme.s3.us-east.cloud-object-storage.appdomain.cloud/UPCH/422ca812-4555-4692-a386-2dcb7ffd5439/logo-upch-chatbot.png"
 st.image(imagen_url, width=100)  # Ajusta el ancho de la imagen según sea necesario
 
 # Añadir el título
