afaacd6c535d9979dcbe5eb6cb01c97eab6691f4
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 20:00:19 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,25 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-# Cargar el archivo CSV directamente
 url_csv = "https://raw.githubusercontent.com/Alexandeeer1/Proyecto_matricula/7cd2140758281c22508e80f81d4e78c34360d983/uss_pass.csv"
 df = pd.read_csv(url_csv)
-
 st.markdown("<h1 style='text-align: center;'>Mi Página en Streamlit</h1>", unsafe_allow_html=True)
-
 url_imagen = "https://intranet.upch.edu.pe/account/static/img/upch/logo/isotipo.jpg"
 st.markdown(f'<div style="text-align: center; border-radius: 20px; overflow: hidden; margin-bottom: 50px;"><img src="{url_imagen}" width="200" style="border-radius: 20px;"></div>', unsafe_allow_html=True)
-
 st.markdown("<h2 style='text-align: center;'>Iniciar Sesión</h2>", unsafe_allow_html=True)
+#####################################################################################
 
-# Formulario de login
 with st.form(key='login_form'):
     username = st.text_input("Usuario")
     password = st.text_input("Contraseña", type="password")
     submit_button = st.form_submit_button(label='Iniciar Sesión')
-
     if submit_button:
-        # Verificar credenciales en el DataFrame
         if (df['Usuarios'] == int(username)).any() and (df['Contraseña'] == password).any():
             st.success("¡Inicio de sesión exitoso!")
         else:
