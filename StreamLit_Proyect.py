e0789261055abb229fc8926be09940af6a169f84
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 19:26:45 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-# Añadir la imagen centrada
-imagen_url = "https://intranet.upch.edu.pe/account/static/img/upch/logo/isotipo.jpg"
+# Añadir la imagen desde GitHub
+imagen_url = "https://github.com/Alexandeeer1/Proyecto_matricula/raw/main/Logo_cayetano.jpg"
 st.markdown(
     f'<div style="text-align:center"><img src="{imagen_url}" width="100"></div>',
     unsafe_allow_html=True
