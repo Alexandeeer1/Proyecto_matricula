5b1e04080c4c34574004af9f571a87fccdc1c464
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-04-26 20:08:26 -0500

    Update StreamLit_Proyect.py

--- StreamLit_Proyect.py
+++ StreamLit_Proyect.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 
@@ -7,17 +6,27 @@ df = pd.read_csv(url_csv)
 st.markdown("<h1 style='text-align: center;'>PORTAL DE MATRICULA CAYETANO</h1>", unsafe_allow_html=True)
 url_imagen = "https://intranet.upch.edu.pe/account/static/img/upch/logo/isotipo.jpg"
 st.markdown(f'<div style="text-align: center; border-radius: 20px; overflow: hidden; margin-bottom: 50px;"><img src="{url_imagen}" width="200" style="border-radius: 20px;"></div>', unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center;'>INICIE SESIÓN</h2>", unsafe_allow_html=True)
+
 #####################################################################################
+# Página de inicio de sesión
+def login_page():
+    st.markdown("<h2 style='text-align: center;'>INICIE SESIÓN</h2>", unsafe_allow_html=True)
+    with st.form(key='login_form'):
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        submit_button = st.form_submit_button(label='Inicie Sesión')
+        if submit_button:
+            if (df['Usuarios'] == int(username)).any() and (df['Contraseña'] == password).any():
+                st.success("¡Inicio de sesión exitoso!")
+                # Llamar a la función de la nueva página
+                logged_in_page()
+            else:
+                st.error("Usuario o contraseña incorrectos")
 
-with st.form(key='login_form'):
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    submit_button = st.form_submit_button(label='Inicie Sesión')
-    if submit_button:
-        if (df['Usuarios'] == int(username)).any() and (df['Contraseña'] == password).any():
-            st.success("¡Inicio de sesión exitoso!")
-        else:
-            st.error("Usuario o contraseña incorrectos")
+# Página después de iniciar sesión
+def logged_in_page():
+    st.markdown("<h2 style='text-align: center;'>Bienvenido, usuario</h2>", unsafe_allow_html=True)
+    st.write("Aquí puedes agregar el contenido de la página después de iniciar sesión")
 
-
+# Mostrar la página de inicio de sesión por defecto
+login_page()
